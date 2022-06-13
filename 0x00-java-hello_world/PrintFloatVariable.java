2884cd66eb55855baa8aa32ae1867f1558de66e0
Melissa Lewartoski Wong <CIANDT\mlewartoski@lnb036080cwb.ciandt.global>
2022-06-13 14:45:12 -0300

    task 2, print float variable #3

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,5 +1,6 @@
 public class PrintFloatVariable {
     public static void main(String[] args){
-        float pi = 3.14159F; System.out.printf("Valor de pi: %.3f\n", pi);
+        float pi = 3.14159F;
+        System.out.printf("Valor de pi: %.3f\n", pi);
     }
 }
