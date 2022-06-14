f9fbdf61c7c47411b7c2a84f95041de7c20411c9
mlewartoski <mlewartoski@ciandt.com>
2022-06-14 12:46:13 -0300

    task 3 #0

---
+++ 0x04/status/PersonagemGame.java
@@ -0,0 +1,38 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public int getSaudeAtual(){
+        return this.saudeAtual;
+    }
+    public void setSaudeAtual(int saudeAtual){
+        this.saudeAtual = saudeAtual;
+        if (this.saudeAtual == 0) {
+            this.status = "morto";
+        } else {
+            this.status = "vivo";
+        }
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public String getStatus() {
+        return this.status;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void tomarDano(int quantidadeDeDano) {
+        int saudeNova = this.saudeAtual - quantidadeDeDano;
+        if (saudeNova < 0) { saudeNova = 0; }
+        setSaudeAtual(saudeNova);
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        int saudeNova = this.saudeAtual + quantidadeDeCura;
+        if (saudeNova > 100) { saudeNova = 100; }
+        setSaudeAtual(saudeNova);
+    }
+
+}
