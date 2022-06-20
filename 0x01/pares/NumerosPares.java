f65d5c0988aa66e387a15a9b53ea75c0cf41f948
mlewartoski <mlewartoski@ciandt.com>
2022-06-20 13:03:31 -0300

    estruturas, decisão e repetição - task 1 #0

---
+++ 0x01/pares/NumerosPares.java
@@ -0,0 +1,9 @@
+public class NumerosPares {
+    public static void main(String[] args) {
+        int i;
+        for (i = 0; i < 98 ; i += 2){
+            System.out.printf("%d, ", i);
+        }
+        System.out.println(i);
+    }
+}
