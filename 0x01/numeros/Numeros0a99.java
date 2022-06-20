1d2d19ae7e1d6f8522e0ccdadf65375d7a0379ee
mlewartoski <mlewartoski@ciandt.com>
2022-06-20 12:59:22 -0300

    estruturas, decisão e repetição - task 0 #0

---
+++ 0x01/numeros/Numeros0a99.java
@@ -0,0 +1,9 @@
+public class Numeros0a99 {
+    public static void main (String[] args) {
+        int i;
+        for (i = 0; i < 99 ; i++){
+            System.out.printf("%d, ", i);
+        }
+        System.out.println(i);
+    }
+}
