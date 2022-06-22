aed6d268b5e9f836135ace16ded5968304b2ad36
mlewartoski <mlewartoski@ciandt.com>
2022-06-22 09:50:18 -0300

    herança - task 4 #0

---
+++ 0x05/quadrado/Quadrado.java
@@ -0,0 +1,22 @@
+public class Quadrado extends Retangulo {
+    private double lado;
+
+    @Override
+    public String toString() {
+        return String.format("[Quadrado] %.2f", lado);
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        if (lado < 0.0) {
+            throw new IllegalArgumentException("Lado deve ser maior ou igual a 0");
+        } else {
+            this.lado = lado;
+            this.altura = lado;
+            this.largura = lado;
+        }
+    }
+}
