fd64bbb4a310017a1ba1cc1671cf683144a157ae
mlewartoski <mlewartoski@ciandt.com>
2022-06-27 14:27:10 -0300

    exercícios herança - task 0 #0

---
+++ Heranca_Exercicios/pedido_livraria/ItemPedido.java
@@ -0,0 +1,21 @@
+package pedido_livraria;
+
+import produtos.*;
+
+public class ItemPedido {
+    private int quantidade;
+    private Produto produto;
+
+    public ItemPedido(Produto produto, int quantidade){
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+}
---
+++ Heranca_Exercicios/pedido_livraria/Pedido.java
@@ -0,0 +1,24 @@
+package pedido_livraria;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Pedido {
+    private double percentualDesconto;
+    private ItemPedido[] itens;
+
+    public Pedido(int percentualDesconto, ItemPedido[] itens){
+        this.percentualDesconto = percentualDesconto;
+        this.itens = itens;
+    }
+    public double calcularTotal(){
+        double valorTotal = 0;
+        for (ItemPedido item:itens) {
+            valorTotal += item.getQuantidade() * item.getProduto().obterPrecoLiquido();
+        }
+        BigDecimal bd = new BigDecimal(Double.toString(valorTotal));
+        bd = bd.setScale(2, RoundingMode.HALF_DOWN);
+        valorTotal = bd.doubleValue();
+        return valorTotal-(percentualDesconto*valorTotal/100);
+    }
+}
