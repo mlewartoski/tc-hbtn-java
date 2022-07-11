f4b33a7fb6dd848e1aca56d288de41b37cf90c81
mlewartoski <mlewartoski@ciandt.com>
2022-07-11 15:21:33 -0300

    lambda exercícios - task 1 #1, renomear param

--- Lambda_Exercicios/predicado/ConsultaProdutos.java
+++ Lambda_Exercicios/predicado/ConsultaProdutos.java
@@ -3,11 +3,10 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class ConsultaProdutos {
-
-    public static ArrayList<Produto> filtrar(List<Produto> produtos, Predicate<Produto> filtro){
+    public static ArrayList<Produto> filtrar(List<Produto> produtos, Predicate<Produto> predicate){
         ArrayList<Produto> novosProdutos = new ArrayList<>();
         produtos.forEach((produto) -> {
-            if (filtro.test(produto)) {
+            if (predicate.test(produto)) {
                 novosProdutos.add(produto);
             }
         });
