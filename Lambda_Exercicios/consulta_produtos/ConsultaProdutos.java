ce7e6a85bcc17f2a78dda7c76e9bf57d52fdb7e9
mlewartoski <mlewartoski@ciandt.com>
2022-07-11 15:04:50 -0300

    lambda exercícios - task 0 #3, retirando uma linha

--- Lambda_Exercicios/consulta_produtos/ConsultaProdutos.java
+++ Lambda_Exercicios/consulta_produtos/ConsultaProdutos.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ConsultaProdutos {
-
     public static ArrayList<Produto> filtrar(List<Produto> produtos, CriterioFiltro filtro){
         ArrayList<Produto> novosProdutos = new ArrayList<>();
         produtos.forEach((produto) -> {
