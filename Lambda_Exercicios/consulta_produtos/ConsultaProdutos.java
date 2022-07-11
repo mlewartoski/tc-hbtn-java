bc906e4c592f559c85b614d6ac11ff20ec55ff5d
mlewartoski <mlewartoski@ciandt.com>
2022-07-11 15:31:18 -0300

    lambda exercícios - task 0 #7, de arraylist para list

--- Lambda_Exercicios/consulta_produtos/ConsultaProdutos.java
+++ Lambda_Exercicios/consulta_produtos/ConsultaProdutos.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ConsultaProdutos {
-    public static ArrayList<Produto> filtrar(List<Produto> produtos, CriterioFiltro criterioFiltro){
-        ArrayList<Produto> novosProdutos = new ArrayList<>();
+    public static List<Produto> filtrar(List<Produto> produtos, CriterioFiltro criterioFiltro){
+        List<Produto> novosProdutos = new ArrayList<>();
         produtos.forEach((produto) -> {
             if (criterioFiltro.testar(produto)) {
                 novosProdutos.add(produto);
