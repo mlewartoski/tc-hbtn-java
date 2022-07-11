546bf3fdaacba48a37ce20221e39c70a1b72422b
mlewartoski <mlewartoski@ciandt.com>
2022-07-11 15:33:54 -0300

    lambda exercícios - task 0 #2, de arraylist para list

--- Lambda_Exercicios/predicado/ConsultaProdutos.java
+++ Lambda_Exercicios/predicado/ConsultaProdutos.java
@@ -3,8 +3,8 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class ConsultaProdutos {
-    public static ArrayList<Produto> filtrar(List<Produto> produtos, Predicate<Produto> predicate){
-        ArrayList<Produto> novosProdutos = new ArrayList<>();
+    public static List<Produto> filtrar(List<Produto> produtos, Predicate<Produto> predicate){
+        List<Produto> novosProdutos = new ArrayList<>();
         produtos.forEach((produto) -> {
             if (predicate.test(produto)) {
                 novosProdutos.add(produto);
