df8a5f603a05e5685908b23b7ecff448e5f3177e
mlewartoski <mlewartoski@ciandt.com>
2022-07-11 14:46:44 -0300

    lambda exercícios - task 0 #1

--- Lambda_Exercicios/consulta_produtos/ConsultaProdutos.java
+++ Lambda_Exercicios/consulta_produtos/ConsultaProdutos.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class ConsultaProdutos {
 
-    public static ArrayList<Produto> filtrar(List<Produto> produtos, CriterioFiltro filtro){
+    static ArrayList<Produto> filtrar(List<Produto> produtos, CriterioFiltro filtro){
         ArrayList<Produto> novosProdutos = new ArrayList<>();
         produtos.forEach((produto) -> {
             if (filtro.testar(produto)) {
