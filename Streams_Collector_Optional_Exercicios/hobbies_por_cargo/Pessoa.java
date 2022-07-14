3bbf97d2c2d24af9804d2d702e55067427fd5388
mlewartoski <mlewartoski@ciandt.com>
2022-07-14 09:38:12 -0300

    streams, collectors, optional exercicios - task 4 #0

---
+++ Streams_Collector_Optional_Exercicios/hobbies_por_cargo/Pessoa.java
@@ -0,0 +1,79 @@
+import java.util.List;
+
+public class Pessoa implements Comparable<Pessoa> {
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+    private List<String> hobbies;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario, List<String> hobbies) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+        this.hobbies = hobbies;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%d] %s %s %d R$ %f", codigo, nome, cargo, idade, salario);
+    }
+
+
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    @Override
+    public int compareTo(Pessoa p) {
+        return this.nome.compareTo(p.getNome());
+    }
+
+    public List<String> getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(List<String> hobbies) {
+        this.hobbies = hobbies;
+    }
+}
